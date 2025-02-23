966fbd22115d67cd3897fe1877b8937be60d1e98
izumigoto <123097688+izumigoto@users.noreply.github.com>
2025-02-23 18:37:12 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', backgroundColor: '#f4f4f4', fontFamily: 'Arial, sans-serif' }}>
       <h1 style={{ color: '#333', fontSize: '2.5rem', marginBottom: '10px' }}>Stow Mock API</h1>
+      <h2 style{{ color:'#333' , fontSize:'2.5rem', marginBottom:'10px'}}>S.Lakshmi Vignesh-W3shi(HackerOne)</h2>
       <p style={{ color: '#555', fontSize: '1.2rem', textAlign: 'center', maxWidth: '600px' }}>
         This is a mock API for simulating responses from <code style={{ backgroundColor: '#ddd', padding: '2px 5px', borderRadius: '4px' }}>qastg.api.stow-protocol.com</code>.
       </p>
